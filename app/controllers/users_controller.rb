a9a9e1a067c4778265760792e6ed2bbbcbc94613
vagrant <vagrant@localhost.localdomain>
2020-07-18 09:55:56 +0000

    2st

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       @book = Book.new
       @books = @user.books
